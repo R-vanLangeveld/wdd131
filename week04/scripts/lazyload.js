0a82c35794d3fcf97aa3353ee1cd10d64d7e40b5
R-vanLangeveld <ryndee25@gmail.com>
2024-10-11 11:19:49 -0600

    Learning Activites and Filtered Temples

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,5 @@
+const today = new Date();
+
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+var lastModified = document.lastModified;
+document.getElementById("modified").innerHTML = lastModified;
